d230601bb525d4ed7a36d79696d8f61ac2ecc8ca
Nirush Man Shrestha <nirushstha@gmail.com>
2026-04-02 14:12:31 +0545

    feat(lib): add app_input_field.dart

---
+++ lib/shared/widgets/app_input_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AppInputField extends StatefulWidget {
+  const AppInputField({
+    super.key,
+    required this.label,
+    this.hint,
+    this.prefixIcon,
+    this.isPassword = false,
+    this.controller,
+    this.errorText,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction = TextInputAction.next,
+    this.onChanged,
+    this.validator,
+    this.maxLines = 1,
+  });
+
+  final String label;
+  final String? hint;
+  final IconData? prefixIcon;
+  final bool isPassword;
+  final TextEditingController? controller;
+  final String? errorText;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final ValueChanged<String>? onChanged;
+  final FormFieldValidator<String>? validator;
+  final int maxLines;
+
+  @override
+  State<AppInputField> createState() => _AppInputFieldState();
+}
+
+class _AppInputFieldState extends State<AppInputField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      textInputAction: widget.textInputAction,
+      onChanged: widget.onChanged,
+      validator: widget.validator,
+      obscureText: widget.isPassword && _obscureText,
+      maxLines: widget.isPassword ? 1 : widget.maxLines,
+      style: Theme.of(context).textTheme.bodyMedium,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        hintText: widget.hint,
+        prefixIcon: widget.prefixIcon == null ? null : Icon(widget.prefixIcon),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: () => setState(() => _obscureText = !_obscureText),
+                icon: Icon(
+                  _obscureText
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                ),
+              )
+            : null,
+        errorText: widget.errorText,
+      ),
+    );
+  }
+}
