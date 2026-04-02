98771d14b419e8c3630acf2047d2c17e2f502d9c
Nirush Man Shrestha <nirushstha@gmail.com>
2026-04-02 14:12:29 +0545

    feat(lib): add app_spacing.dart

---
+++ lib/core/design_system/app_spacing.dart
@@ -0,0 +1,14 @@
+class AppSpacing {
+  static const double xxs = 4;
+  static const double xs = 8;
+  static const double xs2 = 12;
+  static const double sm = 16;
+  static const double md = 24;
+  static const double lg = 32;
+  static const double xl = 40;
+
+  static const double radius = 12;
+  static const double buttonHeight = 44;
+  static const double cardPadding = md;
+  static const double screenPadding = md;
+}
